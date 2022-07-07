c4e00b9cdf605705deb9b728a0b0f23fb248e4fc
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-07-07 15:48:38 +0200

    review: add tests

---
+++ testdata/src/b/c/mock_test.go
@@ -0,0 +1,36 @@
+package c
+
+import (
+	"context"
+	"testing"
+)
+
+// mocktail:Pineapple
+// mocktail:Coconut
+
+func TestName(t *testing.T) {
+	var s Pineapple = newPineappleMock(t).
+		OnHello(Water{}).TypedReturns("a").Once().
+		OnWorld().TypedReturns("a").Once().
+		OnGoo().TypedReturns("", 1, Water{}).Once().
+		OnCoo("", Water{}).TypedReturns(Water{}).
+		TypedRun(func(s string, water Water) {}).Once().
+		Parent
+
+	s.Hello(Water{})
+	s.World()
+	s.Goo()
+	s.Coo(context.Background(), "", Water{})
+
+	fn := func(st Strawberry, stban Strawberry) Pineapple {
+		return s
+	}
+
+	var c Coconut = newCoconutMock(t).
+		OnLoo("a", 1, 2).TypedReturns("foo").Once().
+		OnMoo(fn).TypedReturns("").Once().
+		Parent
+
+	c.Loo("a", 1, 2)
+	c.Moo(fn)
+}
